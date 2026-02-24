322fbf1d639ba8fca69f538ffebcec63bbac4605
sukrittt <sukritsaha27@gmail.com>
2026-02-24 19:31:40 +0530

    feat: cleanup

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
-};
--- src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { DashboardScreen } from './DashboardScreen';
-export { InsightsScreen } from './InsightsScreen';
-export { OnboardingScreen } from './OnboardingScreen';
-export { ReviewQueueScreen } from './ReviewQueueScreen';
-export { SettingsScreen } from './SettingsScreen';
-export { TransactionsScreen } from './TransactionsScreen';
